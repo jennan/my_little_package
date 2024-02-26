476a3bd766b12291be52b8fb21e5f83133973720
Alex Pletzer <alexander@gokliya.net>
2024-02-26 13:12:35 +1300

    + added tests

---
+++ tests/test_talk.py
@@ -0,0 +1,12 @@
+import pytest
+from my_little_package import talk
+
+
+def test_talk():
+	talk.greeting('Adam')
+
+
+# expected to fail
+@pytest.mark.xfail()
+def test_talk_bad():
+    talk.greeting(1)
